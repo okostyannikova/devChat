1b35568749663746dd2d9db16cc23ff257896d07
o.kostyannikova <o.kostyannikova@gmail.com>
2019-01-28 18:27:32 +0200

    feat: automatic checking of the user when the app is loaded

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import firebase from "./firebase";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
+class Root extends Component {
+  componentDidMount = () => {
+    const { history } = this.props;
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        history.push("/");
+      }
+    });
+  };
 
-const Root = () => (
-	<Router>
-		<Switch>
-			<Route exact path="/" component={App} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-		</Switch>
-	</Router>
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
